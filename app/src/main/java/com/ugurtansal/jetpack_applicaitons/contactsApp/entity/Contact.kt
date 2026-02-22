dbede98acf3212d30b65443b35b1ece2079d56c0
Uğur Tansal <ugur.tansal@gmail.com>
2026-02-22 17:34:25 +0300

    Updated Contact . It's available for Entity library

--- app/src/main/java/com/ugurtansal/jetpack_applicaitons/contactsApp/entity/Contact.kt
+++ app/src/main/java/com/ugurtansal/jetpack_applicaitons/contactsApp/entity/Contact.kt
@@ -1,7 +1,15 @@
 package com.ugurtansal.jetpack_applicaitons.contactsApp.entity
 
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
+
+@Entity(tableName = "contacts")
 data class Contact(
-    val contactId:Int,
-    val contactName: String,
-    val contactNumber:Int,
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name="contact_id") @NotNull var contactId:Int,
+    @ColumnInfo(name="contact_name") @NotNull var contactName: String,
+    @ColumnInfo(name="contact_number") @NotNull var contactNumber:Int,
 )
